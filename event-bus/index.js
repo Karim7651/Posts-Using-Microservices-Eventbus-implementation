efbac80a57365989bac62dcb88187153ddb1a5da
Karim Shaaban <karimatef785@gmail.com>
2025-07-26 07:44:08 +0300

    working

--- event-bus/index.js
+++ event-bus/index.js
@@ -9,7 +9,7 @@ app.post('/events', (req, res) => {
   const event = req.body;
   events.push(event);
   //send event to all services
-  axios.post('http://localhost:4000/events', event).catch((err) => {
+  axios.post('http://posts-clusterip-srv:4000/events', event).catch((err) => {
     console.log(err.message);
   }); //posts
   axios.post('http://localhost:4001/events', event).catch((err) => {
